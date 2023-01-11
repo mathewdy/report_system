ef67a2040d4f0e2244d643d6ed7f26b3bc68eaf8
Thadd <58230971+Teejhayzxc@users.noreply.github.com>
2023-01-11 23:39:50 +0800

    commit

--- admin/register.php
+++ admin/register.php
@@ -1,6 +1,6 @@
 <?php
 date_default_timezone_set('Asia/Manila');
-include('connection.php');
+include('../connection.php');
 ?>
 
 <!DOCTYPE html>
@@ -36,21 +36,48 @@ if (isset($_POST['submit'])) {
 
     $username = $_POST['username'];
     $password = $_POST['password'];
-    $user_type = 0;
+    $user_type = 1;
 
     $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-    $register = "INSERT INTO `users`(`username`, `password`, `user_type`, `date_time_created`, `date_time_updated`) 
-    VALUES('$username','$hashed_password','$user_type','$dateCreated','$dateUpdated')";
+
+
+
+
+
+    // $query_user_id = "SELECT * FROM users ORDER BY user_id DESC LIMIT 1";
+    // $run_user_id = mysqli_query($conn, $query_user_id);
+    // if (mysqli_num_rows($run_user_id) > 0) {
+    //     foreach ($run_user_id as $row) {
+    //         $user_id = $row['user_id'];
+    //         $get_number = str_replace("ADM", "", $user_id);
+    //         $id_increment = $get_number + 1;
+    //         $get_string  = str_pad($id_increment, 5, 0, STR_PAD_LEFT);
+
+    //         $id = "ADM" . $get_string;
+
+    $id = "ADM00001";
+
+    $register = "INSERT INTO `users`(`user_type`, `user_id`, `username`, `password`, `date_time_created`, `date_time_updated`) 
+    VALUES ('$user_type','$id','$username','$hashed_password','$dateCreated','$dateUpdated')";
     $run_register = mysqli_query($conn, $register);
 
     if ($run_register) {
         echo ("REGISTERED");
     } else {
 
+
+
         $conn->error;
     }
+
+
+    echo ($register);
 }
+
+
+// }
+// }
 
 
 
