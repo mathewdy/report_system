d2931d075a7ab2460109bd18d6b654d19499dbc0
Raie01 <ryantecling@gmail.com>
2023-01-26 02:49:06 +0800

    Update
    Changes
      - Login and registration design
      - svg added
      - preloader added

--- admin/register.php
+++ admin/register.php
@@ -6,7 +6,6 @@ ob_start();
 
 
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -14,59 +13,128 @@ ob_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="../src/css/preloader.css">
+    <link rel="stylesheet" href="../src/css/template.css">
     <title>Document</title>
 </head>
+<style>
+    .bg{
+        position: fixed;
+        bottom: 0;
+        left: 0;
+        z-index: -1;
+    }
+    .blob{
+        position: absolute;
+        height: 50rem;
+        top: -50%;
+        right: -25%;
+        z-index: -2;
+    }
+    body{
+        overflow-x: hidden;
+    }
+    .avatar{
+        height: 100px;
+    }
+</style>
+<body class="d-none">
+    <div class="preload-wrapper">
+        <div class="spinner-border text-info" role="status">
+            <span class="visually-hidden">Loading...</span>
+        </div>
+    </div>
+    <img class="bg" src="../src/img/bg.svg" alt="">
+    <img class="blob" src="../src/img/blob.svg" alt="">
 
-<body>
+    <div class="container-xxl">
+        <div class="row d-flex justify-content-center align-items-center pt-5">
+            <div class="col-lg-10">    
+                <div class="row shadow">
+                    <div class="col-lg-6 p-5 d-flex justify-content-center align-items-center" style="background: #7694D4;">
+                        <img class="card-img-top" src="../src/img/draw.svg" alt="">
+                    </div>
+                    <div class="col-lg-6 bg-white">
+                        <div class="p-5">
+                            <p class="h3 text-center text-muted mb-4">Registration</p>
+                            <hr class="featurette-divider">
+                            <form action="" method="POST" enctype="multipart/form-data">
+                                <div class="row">
+                                    <!-- <img class="card-img-top avatar" src="../src/img/avatar.svg" alt=""> -->
 
-    <h1>Registration</h1>
-    <form action="" method="POST" enctype="multipart/form-data">
+                                    <div class="col-lg-6">
+                                        <div class="row">
+                                            <div class="col-lg-12 mb-3">
+                                                <label for="">First Name</label>
+                                                <input type="text" class="form-control form-control-sm" name="first_name">
+                                            </div>
+                                            <div class="col-lg-12 mb-3">
+                                                <label for="">Middle Name</label>
+                                                <input type="text" class="form-control form-control-sm" name="middle_name">
+                                            </div>
+                                            <div class="col-lg-12 mb-3">
+                                                <label for="">Last Name</label>
+                                                <input type="text" class="form-control form-control-sm" name="last_name">
+                                            </div>
+                                            <div class="col-lg-12 mb-3">
+                                                <label for="">Date of Birth</label>
+                                                <input type="date" class="form-control form-control-sm" name="date_of_birth" id="">
 
-        <h2>Log In Info</h2>
-        <label for="">Email</label>
-        <br>
-        <input type="text" name="email">
-        <br>
-        <label for="">Password</label>
-        <br>
-        <input type="password" name="password" id="">
-
-        <h2>Personal Info</h2>
-        <label for="">First Name</label>
-        <br>
-        <input type="text" name="first_name">
-        <br>
-        <label for="">Middle Name</label>
-        <br>
-        <input type="text" name="middle_name">
-        <br>
-        <label for="">Last Name</label>
-        <br>
-        <input type="text" name="last_name">
-        <br>
-        <label for="">Date of Birth</label>
-        <br>
-        <input type="date" name="date_of_birth" id="">
-        <br>
-        <label for="">Address</label>
-        <br>
-        <input type="text" name="address">
-        <br>
-        <label for="">Barangay</label>
-        <br>
-        <input type="text" name="barangay">
-        <br>
-        <label for="">Dilg ID</label>
-        <br>
-        <input type="text" name="dilg_id">
-        <br>
-        <label for="">Image</label>
-        <input type="file" name="image" id="">
-        <br>
-        <input type="submit" name="register" value="Register">
-
-
-    </form>
+                                            </div>
+                                            <div class="col-lg-12 mb-3">
+                                                <label for="">Address</label>
+                                                <input type="text" class="form-control form-control-sm" name="address">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6">
+                                        <div class="row">
+                                            <div class="col-lg-12 mb-3">
+                                                <label for="">Barangay</label>
+                                                <input type="text" class="form-control form-control-sm" name="barangay">
+                                            </div>
+                                            <div class="col-lg-12 mb-3">
+                                                <label for="">DILG ID</label>
+                                                <input type="text" class="form-control form-control-sm" name="dilg_id">
+                                            </div>
+                                            <div class="col-lg-12 mb-3">
+                                                <label for="">Image</label>
+                                                <input type="file" class="form-control form-control-sm" name="image" id="">
+                                            </div>
+                                            <div class="col-lg-12 mb-3">
+                                                <label for="">Email</label>
+                                                <input type="text" class="form-control form-control-sm" name="email">
+                                            </div>
+                                            <div class="col-lg-12 mb-4">
+                                                <label for="">Password</label>
+                                                <input type="password" class="form-control form-control-sm" name="password" id="">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <hr class="featurette-divider">
+                                    <div class="col-lg-12 text-center">
+                                        <input class="btn btn-md mb-1 btn-primary w-100" style="background: #7694D4; outline:#7694D4; border: #7694D4; border-radius: 0;" type="submit" name="register" value="Register">
+                                        <a class="text-decoration-none" href="login.php">Log In</a>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script>
+        $(window).on('load', function(){
+            $('body').removeClass('d-none');
+            $('.preload-wrapper').fadeOut(1000);
+        })
+    </script>
 </body>
 
 </html>
