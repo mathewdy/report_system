85700c2e478f88061acf648a692fdd0aae1e508c
mathewdy <mathewdalisay@gmail.com>
2023-01-30 06:05:47 +0800

    Updated

--- admin/add-note.php
+++ admin/add-note.php
@@ -134,7 +134,7 @@ $note_link = "add-note.php";
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
 
-  <script>
+  <!-- <script>
     $('textarea#tiny').tinymce({
       height: 500,
       menubar: true,
@@ -145,7 +145,7 @@ $note_link = "add-note.php";
       ],
       toolbar: 'undo redo | a11ycheck casechange blocks | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist checklist outdent indent | removeformat | code table help | save | restoredraft'
     });
-  </script>
+  </script> -->
 
 
 
