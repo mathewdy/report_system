14a216966a789768e5a0c20c4ac2debf6c562ea8
Raie01 <ryantecling@gmail.com>
2023-02-02 08:38:24 +0800

    Update home.php

--- users/home.php
+++ users/home.php
@@ -213,11 +213,10 @@ $email = $_SESSION['email'];
 	</div>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
 <script src="../src/js/template-2.js"></script>
-<script src="../src/js/preload.js"></script>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
 <script src="https://cdn.jsdelivr.net/npm/@tinymce/tinymce-jquery@1/dist/tinymce-jquery.min.js"></script>
-
+<script src="../src/js/preload.js"></script>
 </body>
 
 </html>
